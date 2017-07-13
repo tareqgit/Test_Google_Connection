45bcddfc5763ea258c2d8e1c67088739f3a25063
tareq Islam <mti.tareq.git@gmail.com>
2017-07-14 03:42:40 +0600

    added_old_files

---
+++ Assets/My_Files/DataHelper.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+using System.Collections.Generic;
+using GooglePlayGames;
+using UnityEngine.UI;
+
+public class DataHelper : MonoBehaviour{
+    bool IsConnectedToGoogleServices;
+    public Text LOg;
+    void Awake()
+    {
+       
+        PlayGamesPlatform.DebugLogEnabled = true;
+    }
+    
+    public void Start()
+    {
+        PlayGamesPlatform.Activate();
+
+
+       Social.localUser.Authenticate((bool success) =>
+        {
+            if (success)
+            {
+                LOg.text = "Success fully LOgged";
+                Debug.Log("Success fully LOgged In");
+                //Do Something
+            }
+            else
+            {
+                LOg.text = "Sorry, can't LOgged";
+            }
+        });
+       
+    }
+
+    public bool ConnectToGoogleServices()
+    {
+        if (!IsConnectedToGoogleServices)
+        {
+            Social.localUser.Authenticate((bool success) =>
+            {
+                IsConnectedToGoogleServices = success;
+            });
+        }
+        return IsConnectedToGoogleServices;
+    }
+
+    public void ToAchivement()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Social.ShowAchievementsUI();
+        }
+        else
+        {
+            //show Error
+        }
+    }
+}
---
+++ Assets/My_Files/buttonController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class buttonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public int i = 0;
+        bool _pressed = false;
+    public float multiplier;
+    public GameObject any;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _pressed = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _pressed = false;
+        }
+
+        void Update()
+        {
+            if (!_pressed)
+                return;
+       
+        i++;
+      any.transform.Translate( Vector3.right *multiplier*Time.deltaTime);
+     //  float a=Time.deltaTime;
+      //a = Mathf.Clamp(a, -Screen.width / 2, Screen.width / 2);
+        //any.transform.position += new Vector3( a, any.transform.position.y, any.transform.position.z);
+        Debug.Log("clicked");
+            // DO SOMETHING HERE
+        }
+    
+}
